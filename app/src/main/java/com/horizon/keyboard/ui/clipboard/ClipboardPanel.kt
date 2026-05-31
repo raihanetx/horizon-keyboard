00d48e82ed8b94433f41c19069eed407ec396e58
Horizon Keyboard <horizon@keyboard.dev>
2026-05-31 04:01:40 +0000

    Fix: add @OptIn for experimental HorizontalPager API

--- app/src/main/java/com/horizon/keyboard/ui/clipboard/ClipboardPanel.kt
+++ app/src/main/java/com/horizon/keyboard/ui/clipboard/ClipboardPanel.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -30,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.horizon.keyboard.data.clipboard.ClipboardItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ClipboardPanel(
     recentItems: List<ClipboardItem>,
